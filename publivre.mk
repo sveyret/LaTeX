d3b04cf015a8168f5785effa0f85c6c9115ffb14
Stéphane Veyret <stephane.veyret@neptura.org>
2017-08-14 20:56:02 +0200

    Use default makefile to be included

---
+++ publivre.mk
@@ -0,0 +1,44 @@
+all: draft print ebook
+
+draft: $(DRAFT)
+
+print: $(PRINT)
+
+ebook: $(EBOOK)
+
+%.draft.pdf: %.tex
+	TEXINPUTS=".:$(LATEXFILES):" latexmk -pdf -jobname=$(basename $@) -pdflatex="pdflatex -jobname=$(basename $@) '\\newcommand\\Brouillon{\\brouillontrue} \\input{%S}'" $<
+	latexmk -jobname=$(basename $@) -c
+	rm -f $(basename $@).som
+
+%.pdf %.data: %.tex
+	TEXINPUTS=".:$(LATEXFILES):" latexmk -pdf -pdflatex=pdflatex $<
+	latexmk -c
+	rm -f $(basename $@).som
+
+%.cover.pdf: %.sla %.data
+	scribus -g -ns $< -py $(LATEXFILES)/autoPDF.py $@ $*.sla $*.data
+
+%.cover.ppm: %.cover.pdf
+	pdftoppm -singlefile -r $(COVERRES) $< >$@
+
+%.cover.png: %.cover.ppm
+	echo '(extract-cover "$<" "$@" $(COVERRES) $(COVERCUT)) (gimp-quit 0)' | cat $(LATEXFILES)/extractCover.scm - | gimp -i -b -
+
+%.html %.css %.opf: %.tex
+	TEXINPUTS=".:$(LATEXFILES):" mk4ht htlatex $< "xhtml,fn-in,charset=utf-8" " -cunihtf -utf8"
+	rm -f $*.4ct $*.4tc $*.aux $*.dvi $*.idv $*.lg $*.log $*.tmp $*.xref
+
+%.epub: %.html %.css %.opf %.cover.png
+	ebook-convert $< $@ --embed-all-fonts --cover $*.cover.png --from-opf $*.opf
+
+clean:
+	rm -f *~
+	rm -f *.data *.html *.css *.opf
+
+mrproper: clean
+	rm -f $(DRAFT) $(PRINT) $(EBOOK)
+
+.INTERMEDIATE : *.data *.html *.css *.opf
+
+.PHONY : all draft print ebook clean mrproper
