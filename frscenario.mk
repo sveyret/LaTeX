c621bd88d33cafd7d558dfc18df2474a3ada412f
Stéphane Veyret <stephane.veyret@neptura.org>
2017-12-02 21:15:59 +0100

    Control nbsp and french guillemots

--- frscenario.mk
+++ frscenario.mk
@@ -1,11 +1,24 @@
 PRINT=$(BASENAME).pdf
 WEB=$(BASENAME).html $(BASENAME).css
 
+define TEST_SEQUENCE=
+perl -ne 'BEGIN{$$skip=1;$$found=0;} $$skip=0 if(m/begin\s*{document}/); if(!$$skip && m/$(1)/){print;$$found=1;} END{exit $$found}' $(2)
+endef
+
+define TEST_TEX=
+echo \
+&& echo " * Guillemets ouvrants suivis d'une espace insécable (utiliser «~ pour passer outre) :" \
+&& $(call TEST_SEQUENCE,«\xC2\xA0,$(1)) \
+&& echo " * Guillemets fermants précédés d'une espace insécable (utiliser ~» pour passer outre) :" \
+&& $(call TEST_SEQUENCE,\xC2\xA0»,$(1))
+endef
+
 print: $(PRINT)
 
 web: $(WEB)
 
 %.pdf: %.tex
+	@$(call TEST_TEX,$<)
 	TEXINPUTS=".:$(LATEXFILES):" latexmk -pdf -pdflatex=pdflatex $<
 	latexmk -c
 
